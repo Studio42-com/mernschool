24182797dd3cf94e0c046c6c013ce7930a483580
Rami Mikhail <webdev67@gmail.com>
2022-02-09 22:59:04 -0500

    Updated profile form jsx

--- src/components/ProfileForm/ProfileForm.jsx
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -3,46 +3,42 @@ import { Component } from "react";
 
 class ProfileForm extends Component {
   state = {
-    name: '',
-    firstName: '',
-    lastName: '',
-    streetAddress: '',
-    addressCity: '',
-    addressState: '',
-    addressZip: '',
-    error: ''
-  }
+    name: "",
+    firstName: "",
+    lastName: "",
+    streetAddress: "",
+    addressCity: "",
+    addressState: "",
+    addressZip: "",
+    error: "",
+  };
 
   handleSubmit = (evt) => {
     evt.preventDefault();
     try {
-        const data = new FormData(evt.target);
-        console.log(data);
-        console.log(data.get('firstName'));
-        var object = {};
-        data.forEach((value, key) => object[key] = value);
-        var json = JSON.stringify(object);
-        console.log(json);
-        const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: json
+      const data = new FormData(evt.target);
+      console.log(data);
+      console.log(data.get("firstName"));
+      var object = {};
+      data.forEach((value, key) => (object[key] = value));
+      var json = JSON.stringify(object);
+      console.log(json);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: json,
       };
-      fetch('http://localhost:3001/api/profile/save', requestOptions)
-          .then(response => response.json())
-          .then(data => console.log(data));
-
+      fetch("http://localhost:3001/api/profile/save", requestOptions)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     } catch {
       // An error occurred...
-
     }
-  }
-
+  };
 
   retrieveEmail = () => {
     return localStorage.getItem("email");
-    };
-
+  };
 
   render() {
     // const disable = this.state.password !== this.state.confirm;
@@ -51,27 +47,57 @@ class ProfileForm extends Component {
         <div className="form-container">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <label>First Name</label>
-            <input type="text" name="firstName" placeholder="Enter your first name" required />
+            <input
+              type="text"
+              name="firstName"
+              placeholder="Enter your first name"
+              required
+            />
             <label>Last Name</label>
-            <input type="text" name="lastName" placeholder="Enter your last name" required />
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Enter your last name"
+              required
+            />
             <label>Street Address</label>
-            <input type="text" name="streetAddress" placeholder="Street address" required />
+            <input
+              type="text"
+              name="streetAddress"
+              placeholder="Street address"
+              required
+            />
             <label>City</label>
-            <input type="text" name="addressCity" placeholder="Enter your City" required />
+            <input
+              type="text"
+              name="addressCity"
+              placeholder="Enter your City"
+              required
+            />
             <label>State</label>
             <select type="text" name="addressState" required>
-                <option value="CA">California</option>
+              <option value="CA">California</option>
             </select>
             <label>State</label>
-            <input type="number" maxLength="5" placeholder="00000" minLength="5" name="addressZip" />
-            <input type="hidden" name="email" value={()=>localStorage.getItem("email")} />
+            <input
+              type="number"
+              maxLength="5"
+              placeholder="00000"
+              minLength="5"
+              name="addressZip"
+            />
+            <input
+              type="hidden"
+              name="email"
+              value={localStorage.getItem("email")}
+            />
 
             <button type="submit">Create Profile</button>
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
       </div>
-    )
+    );
   }
 }
 
