f19c3eb58bc6aa1213eb6c32c6d0350c54f0a50f
Rami Mikhail <webdev67@gmail.com>
2022-02-11 22:37:18 -0500

    Fixed css issue

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 
-
 function NavBar({ user, setUser }) {
-
   function handleLogOut() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("email");
     userService.logOut();
     setUser(null);
   }
@@ -17,12 +17,12 @@ function NavBar({ user, setUser }) {
       &nbsp; | &nbsp; */}
       <span>Welcome, {user.name}!</span>
       &nbsp; | &nbsp;
-      <Link onClick={handleLogOut} to=""><button>Log Out</button></Link>
-
+      <Link onClick={handleLogOut} to="">
+        <button>Log Out</button>
+      </Link>
       <hr />
     </nav>
-
-  )
+  );
 }
 
 export default NavBar;
--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -3,80 +3,79 @@ import { Component, useEffect } from "react";
 import NavBar2 from "../../components/NavBar2/NavBar2";
 import NavBar from "../../components/NavBar/NavBar";
 
-import styles from './User.css';
+import styles from "./User.css";
 
 class User extends Component {
-    componentDidMount() {
-      const data = { email: localStorage.getItem("email") };
-      fetch("/api/profile/read", {
-        method: "POST", // or 'PUT'
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          this.setState(data);
-          console.log(this.state);
-        });
-    }
-  
-    state = {
-      // name: "",
-      firstName: "",
-      lastName: "",
-      streetAddress: "",
-      addressCity: "",
-      addressState: "",
-      addressZip: "",
-      error: "",
-      doRedirect: false,
-    };
-  
-    retrieveEmail = () => {
-      return localStorage.getItem("email");
-    };
-  
-    render() {
-      return (
+  componentDidMount() {
+    const data = { email: localStorage.getItem("email") };
+    fetch("/api/profile/read", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState(data);
+        console.log(this.state);
+      });
+  }
 
-    <>
+  state = {
+    // name: "",
+    firstName: "",
+    lastName: "",
+    streetAddress: "",
+    addressCity: "",
+    addressState: "",
+    addressZip: "",
+    error: "",
+    doRedirect: false,
+  };
 
-       <div>
-        <div id="blackoutline"><h2>Profile Page</h2>
-        This is your Profile Page. Please check for accuracy. If everything is correct, please print the application using the "Print Application" button below.
-        </div>
-        <table>
+  retrieveEmail = () => {
+    return localStorage.getItem("email");
+  };
+
+  render() {
+    return (
+      <>
+        <div className="profile">
+          <div id="blackoutline">
+            <h2>Profile Page</h2>
+            This is your Profile Page. Please check for accuracy. If everything
+            is correct, please print the application using the "Print
+            Application" button below.
+          </div>
+          <table>
             <theader>
-                <th>User Profile Information</th>
-                <th>2</th>
+              <th>User Profile Information</th>
+              <th>2</th>
             </theader>
-      
+
             <tr>
-               
-                <td>
-                    <div id="blackoutline">
-                    <ul>
+              <td>
+                <div id="blackoutline">
+                  <ul>
                     <li>First Name: {this.state.firstName}</li>
                     <li>Last Name: {this.state.lastName}</li>
                     <li>Street: {this.state.streetAddress}</li>
                     <li>City: {this.state.addressCity}</li>
                     <li>State: {this.state.addressState}</li>
                     <li>Zip Code: {this.state.addressZip}</li>
-                    </ul>
-                    </div>
-                </td>
-                <td>[Error] or Show profile information(limited)</td>
+                  </ul>
+                </div>
+              </td>
+              <td>[Error] or Show profile information(limited)</td>
             </tr>
-        </table>
+          </table>
 
-        <NavBar2 />
+          <NavBar2 />
         </div>
-          
-    </>
-    )
-    }
+      </>
+    );
+  }
 }
 
 export default User;
